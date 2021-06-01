8681e8b88491f9762f6ccd2b5f961c1a897b181f
Antonius Joshua <antoniusjoshua47@gmail.com>
2021-06-02 00:55:23 +0700

    [Update] Add Account & Giving Screen

---
+++ lib/ui/screens/giving_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GivingScreen extends StatelessWidget {
+  const GivingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 200,
+      height: 200,
+      child: Text("testing"),
+    );
+  }
+}
